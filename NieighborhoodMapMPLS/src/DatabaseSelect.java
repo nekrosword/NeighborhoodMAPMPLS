9e45d0ac0a26e7aa7c9b6d48cf24c64aaafc00c4
nekrosword <33295352+nekrosword@users.noreply.github.com>
2018-11-27 14:37:05 -0600

    adding connection to db

--- NieighborhoodMapMPLS/src/DatabaseSelect.java
+++ NieighborhoodMapMPLS/src/DatabaseSelect.java
@@ -1,6 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseSelect {
 
 	
+	private final String user ="root";
+	private final String pass = "EB485TacoTuesday1!";
 	private final String jdbcURL = "jdbc:mysql:localhost:3306/MinnNeigh?autoreconnect=true&ampuseSSL=false";
+
+	public void SelectFrom(String neighborhood) {
+		 Statement stmt = null;
+		String sql = "Select crime types from neighborhood left join table on id";
+		 try(Connection con = DriverManager.getConnection(jdbcURL,user, pass);) {
+		        stmt = con.createStatement();
+		        ResultSet rs = stmt.executeQuery(sql);
+		        while (rs.next()) {
+		        
+		        }
+	} catch (SQLException e) {
+		e.getMessage();
+		e.printStackTrace();
+	}
+	}
 }
